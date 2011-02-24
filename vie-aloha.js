6dbc9e753aa904c9ecf5c69da7f7b08794188d09
Johannes Schüth <j.schueth@gentics.com>
2011-02-24 14:31:51 +0100

    Enabled aloha editing non nested rdfa annotated tags

--- vie-aloha.js
+++ vie-aloha.js
@@ -3,7 +3,11 @@
 		this.each(function() {
 			var containerInstance = VIE.ContainerManager.getInstanceForContainer(jQuery(this));
 			containerInstance.editables = {};
-			jQuery(containerInstance.view.el).find('[property]').each(function() {
+			var elements = jQuery(containerInstance.view.el).filter('[property]');
+			if(elements.size() == 0) {
+				elements = jQuery(containerInstance.view.el).find('[property]'); 
+			}
+			elements.each(function() {
 				var containerProperty = jQuery(this);
 				var propertyName = containerProperty.attr('property');
 				containerInstance.editables[propertyName] = new GENTICS.Aloha.Editable(containerProperty);
