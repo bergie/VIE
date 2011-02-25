9b08753bffd4759976050e8fcc18e74383f949fa
Henri Bergius <henri.bergius@iki.fi>
2011-02-25 15:24:08 +0200

    New helper for refreshing an object instance from an editable

--- vie-aloha.js
+++ vie-aloha.js
@@ -18,3 +18,33 @@
         })
     }
 })(jQuery);
+
+if (typeof VIE === 'undefined') {
+    VIE = {};
+}
+
+VIE.AlohaEditable = {
+    refreshFromEditables: function(objectInstance) {
+       var modifiedProperties = {};
+
+        // Go through editables of the model instance
+        jQuery.each(objectInstance.editables, function(propertyName, editableInstance) {
+            if (!editableInstance.isModified()) {
+                // This editable hasn't been modified, skip
+                return false;
+            }
+            // Copy editable contents to the modifiedProperties object
+            modifiedProperties[propertyName] = editableInstance.getContents();
+        });
+
+        if (jQuery.isEmptyObject(modifiedProperties))
+        {
+            // No modified editables for this object, skip
+            return false;
+        }
+
+        // Set the modified properties to the model instance
+        objectInstance.set(modifiedProperties);
+        return true;
+    }
+};
