6f6d76a7986bd2afefa1b51fb7a4c77fa38e3a00
Henri Bergius <henri.bergius@iki.fi>
2011-02-26 13:37:51 +0200

    Only instantiate empty editabled object if it isn't already defined

--- vie-aloha.js
+++ vie-aloha.js
@@ -2,7 +2,9 @@
     $.fn.vieSemanticAloha = function() {
         this.each(function() {
             var containerInstance = VIE.ContainerManager.getInstanceForContainer(jQuery(this));
-            containerInstance.editables = {};
+            if (typeof containerInstance.editables === 'undefined') {
+                containerInstance.editables = {};
+            }
             VIE.ContainerManager.findContainerProperties(this, false).each(function() {
                 var containerProperty = jQuery(this);
                 var propertyName = containerProperty.attr('property');
