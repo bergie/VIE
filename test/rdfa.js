35ef018ed19afcf81162c3ab1beebcdef8cc0c4b
Henri Bergius <henri.bergius@iki.fi>
2011-03-08 19:09:42 +0200

    More unit testing

--- test/rdfa.js
+++ test/rdfa.js
@@ -54,6 +54,7 @@ exports['test global entity'] = function(test) {
     // We should find the dc:creator from this. Unfortunately there is no subject as this isn't on browser
     test.equal(jsonldEntities.length, 1);
     test.equal(jsonldEntities[0]['dc:creator'], 'Jo');
+    test.equal(jsonldEntities[0]['@'], undefined);
     VIE.cleanup();
 
     var html = jQuery('<html><head><title>Jo\'s Blog</title></head><body><h1><span property="dc:creator">Jo</span>\'s blog</h1><p>Welcome to my blog.</p></body></html>');
@@ -77,6 +78,20 @@ exports['test global entity with base URL'] = function(test) {
     test.equal(jsonldEntities.length, 1);
     test.equal(jsonldEntities[0]['dc:creator'], 'Jo');
     test.equal(jsonldEntities[0]['@'], 'http://www.example.org/jo/blog');
+
+    VIE.cleanup();
+    test.done();
+};
+
+exports['test about and anonymous'] = function(test) {
+    var html = jQuery('<html><head><title>Jo\'s Friends and Family Blog</title><link rel="foaf:primaryTopic" href="#bbq" /><meta property="dc:creator" content="Jo" /></head><body><p about="#bbq" typeof="cal:Vevent">I\'m holding<span property="cal:summary">one last summer barbecue</span>, on <span property="cal:dtstart" content="2007-09-16T16:00:00-05:00" datatype="xsd:dateTime">September 16th at 4pm</span>.</p></body></html>');
+
+    var jsonldEntities = VIE.RDFa.readEntities(html);
+
+    test.equal(jsonldEntities.length, 2);
+    test.equal(jsonldEntities[0]['a'], 'cal:Vevent');
+    // FIXME: This should really have the datatype
+    test.equal(jsonldEntities[0]['cal:dtstart'], '2007-09-16T16:00:00-05:00');
 
     VIE.cleanup();
     test.done();
