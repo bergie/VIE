69d94e4117e3980f0f5b6dd58c78daa612f92f06
Henri Bergius <henri.bergius@iki.fi>
2011-03-08 18:34:41 +0200

    Auto-updating view test

--- test/rdfa.js
+++ test/rdfa.js
@@ -19,5 +19,31 @@ exports['test inheriting subject'] = function(test) {
     test.equal(backboneEntities[1].get('foaf:name'), 'Albert Einstein');
     test.equal(backboneEntities[0].get('dbp:conventionalLongName'), 'Federal Republic of Germany');
 
+    VIE.cleanup();
+
+    test.done();
+};
+
+exports['test updating views'] = function(test) {
+    var html = jQuery('<div about="http://dbpedia.org/resource/Albert_Einstein"><span property="foaf:name">Albert Einstein</span><span property="dbp:dateOfBirth" datatype="xsd:date">1879-03-14</span><div rel="dbp:birthPlace" resource="http://dbpedia.org/resource/Germany" /><span about="http://dbpedia.org/resource/Germany" property="dbp:conventionalLongName">Federal Republic of Germany</span></div>');
+
+    var backboneEntities = VIE.RDFaEntities.getInstances(html);
+
+    // Ensure we got the value
+    test.equal(backboneEntities[0].get('dbp:conventionalLongName'), 'Federal Republic of Germany');
+
+    // Change the value
+    backboneEntities[0].set({'dbp:conventionalLongName': 'Switzerland'});
+
+    // Ensure that it was changed in the model
+    test.equal(backboneEntities[0].get('dbp:conventionalLongName'), 'Switzerland');
+
+    // And then the interesting bit, check that it changed in the HTML as well
+    jQuery('[property="dbp:conventionalLongName"]', html).each(function() {
+        test.equal(jQuery(this).html(), 'Switzerland', 'Check that the change actually affected the HTML');
+    });
+
+    VIE.cleanup();
+
     test.done();
 };
