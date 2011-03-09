0c6f56662dbe5ee6c466ef29dd91ece097d6e6ff
Henri Bergius <henri.bergius@iki.fi>
2011-03-09 12:22:16 +0200

    Add a test for parsed relations in case user has a patched version of jsdom

--- test/rdfa.js
+++ test/rdfa.js
@@ -1,8 +1,8 @@
 var jQuery = require('jquery');
 var VIE = require('../vie.js');
 
-// Until https://github.com/tmpvar/jsdom/issues/issue/81 is fixed
-VIE.RDFa.predicateSelector = '[property]';
+// Until https://github.com/tmpvar/jsdom/issues/issue/81 is fixed you need to uncomment the following:
+//VIE.RDFa.predicateSelector = '[property]';
 
 exports['test inheriting subject'] = function(test) {
     var html = jQuery('<div about="http://dbpedia.org/resource/Albert_Einstein"><span property="foaf:name">Albert Einstein</span><span property="dbp:dateOfBirth" datatype="xsd:date">1879-03-14</span><div rel="dbp:birthPlace" resource="http://dbpedia.org/resource/Germany" /><span about="http://dbpedia.org/resource/Germany" property="dbp:conventionalLongName">Federal Republic of Germany</span></div>');
@@ -12,6 +12,7 @@ exports['test inheriting subject'] = function(test) {
 
     test.equal(jsonldEntities[1]['foaf:name'], 'Albert Einstein');
     test.equal(jsonldEntities[0]['dbp:conventionalLongName'], 'Federal Republic of Germany');
+    test.equals(jsonldEntities[1]['dbp:birthPlace'], '<' + jsonldEntities[0]['@'] + '>', "Check that the relation between the person and the birthplace was read correctly");
 
     var backboneEntities = VIE.RDFaEntities.getInstances(html);
     test.equal(backboneEntities.length, 2, "This RDFa defines two entities but they don't get to Backbone");
