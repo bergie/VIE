c63b1521eb0eda3bfdb6f454b127a3a6c804431c
Szaby Grünwald <szaby.gruenwald@salzburgresearch.at>
2011-08-30 19:16:41 +0200

    init components

--- src/Annotatable.js
+++ src/Annotatable.js
@@ -1,11 +1,12 @@
 
-Zart.prototype.Loadable = function(options){
+Zart.prototype.Annotatable = function(options){
     this.options = options;
     this.from = [];
     this.zart = options.zart;
     this.deferred = jQuery.Deferred();
+
     this.success = this.deferred.success;
-    this.from = function(service) {
+    this.using = function(service) {
         // TODO
     }
 }
