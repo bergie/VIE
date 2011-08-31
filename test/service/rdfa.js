b55f50cf09e6b0f712d2b5ac0dbcf6c6125717bf
Henri Bergius <henri.bergius@iki.fi>
2011-08-31 12:54:27 +0300

    Instantiate the service

--- test/service/rdfa.js
+++ test/service/rdfa.js
@@ -2,7 +2,7 @@ module("zart.js - RDFa Service");
 
 asyncTest("Test RDFa parsing", function() {
     var z = new Zart();
-    z.use(z.RdfaService);
+    z.use(new z.RdfaService);
 
     var html = jQuery('<div about="http://dbpedia.org/resource/Albert_Einstein"><span property="foaf:name">Albert Einstein</span><span property="dbp:dateOfBirth" datatype="xsd:date">1879-03-14</span><div rel="dbp:birthPlace" resource="http://dbpedia.org/resource/Germany" /><span about="http://dbpedia.org/resource/Germany" property="dbp:conventionalLongName">Federal Republic of Germany</span></div>');
 
