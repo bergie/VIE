4c9674c3a1fb3a940b015b850ba60157bdda4fdf
Sebastian Germesin <neogermi@googlemail.com>
2011-09-01 17:00:46 +0200

    fixed stop/start within tests

---
+++ test/core/namespaces.js
@@ -0,0 +1,78 @@
+module("Core - Namespaces");
+
+test("Zart namespace", function () {
+    var z = new Zart();
+    
+   ok(z);
+   ok(z.defaultNamespace);
+   ok(z.namespaces);
+   
+   ok(z.namespaces.add);
+   equal(typeof z.namespaces.add, "function");
+   ok(z.namespaces.get);
+   equal(typeof z.namespaces.get, "function");
+   ok(z.namespaces.containsKey);
+   equal(typeof z.namespaces.containsKey, "function");
+   ok(z.namespaces.containsValue);
+   equal(typeof z.namespaces.containsValue, "function");
+   ok(z.namespaces.update);
+   equal(typeof z.namespaces.update, "function");
+   ok(z.namespaces.remove);
+   equal(typeof z.namespaces.remove, "function");
+   ok(z.namespaces.toObj);
+   equal(typeof z.namespaces.toObj, "function");
+   ok(z.namespaces.curie);
+   equal(typeof z.namespaces.curie, "function");
+   ok(z.namespaces.uri);
+   equal(typeof z.namespaces.uri, "function");
+   
+   
+});
+
+test ("Manually adding namespaces", function () {
+    var z = new Zart();
+    
+    var reference = jQuery.extend(z.namespaces.toObj(), {'test' : 'http://this.is.a/test#'});
+
+    z.namespaces.add("test","http://this.is.a/test#");
+    
+    deepEqual(z.namespaces.toObj(), reference, "Manually adding namespaces.");
+    strictEqual(z.namespaces.get("test"), "http://this.is.a/test#", "Manually adding namespaces.");
+});
+
+test ("Manually adding duplicate", function () {
+    var z = new Zart();
+    var reference = jQuery.extend(z.namespaces.toObj(), {'test' : 'http://this.is.a/test#'});
+    z.namespaces.add("test", "http://this.is.a/test#");
+    z.namespaces.add("test", "http://this.is.a/test#");
+    deepEqual(z.namespaces.toObj(), reference, "Manually adding namespaces.");
+    strictEqual(z.namespaces.get("test"), "http://this.is.a/test#", "Manually adding namespaces.");
+});
+
+test ("Manually adding wrong duplicate (key)", function () {
+    var z = new Zart();
+    z.namespaces.add("test", "http://this.is.a/test#");
+    raises(function () {
+        z.namespaces.add("test1", "http://this.is.a/test#");
+    });
+});
+
+test ("Manually adding wrong duplicate (value)", function () {
+    var z = new Zart();
+    z.namespaces.add("test", "http://this.is.a/test#");
+    raises(function () {
+        z.namespaces.add("test", "http://this.is.another/test#");
+    });
+});
+
+test ("Manually removing namespaces", function () {
+    var z = new Zart();
+    z.namespaces.add("test", "http://this.is.a/test#");
+    var reference = z.namespaces.toObj();
+    delete reference["test"];
+     
+    z.namespaces.remove("test");
+   
+    deepEqual(z.namespaces.toObj(), reference, "Manually removing namespaces.");
+    strictEqual(z.namespaces['test'], undefined, "Manually removing namespaces.");
+});
