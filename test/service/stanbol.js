e3fe5771a2da6351acb6cc31041674165d7cd0bf
Szaby Grünwald <szaby.gruenwald@salzburgresearch.at>
2011-09-01 16:07:44 +0200

    Extending stanbolservice tests.

--- test/service/stanbol.js
+++ test/service/stanbol.js
@@ -7,24 +7,19 @@ test("Test stanbol connection", function() {
 
     ok(z.service('stanbol'));
 
-    // Entityhub sites list
-    z.service('stanbol').getSites
 });
 
 test("Zart.js StanbolService - Annotate", function () {
-    
     var elem = $('<p>This is a small test, where Steve Jobs sings a song.</p>');
-    
     var z = new Zart();
-   
     ok (z.StanbolService);
     equal(typeof z.StanbolService, "function");
-    
+
     z.use(new z.StanbolService({url : "http://dev.iks-project.eu:8081"}));
-    
     stop(5000); // 5 second timeout
     z.annotate({element: elem}).using('stanbol').execute().done(function(entities) {
-        console.log(entities);
+
+        // console.log(entities);
 
         ok(entities);
         //ok(entities.length > 0);
@@ -32,3 +27,24 @@ test("Zart.js StanbolService - Annotate", function () {
         start();
     });
 });
+/*
+
+test("Zart.js StanbolService - Fail tests", function () {
+    var elem = $('<p>This is a small test, where Steve Jobs sings a song.</p>');
+    var z = new Zart();
+    ok (z.StanbolService);
+    equal(typeof z.StanbolService, "function");
+
+    z.use(new z.StanbolService({url : "http://wrong-url-or-server-down.com"}));
+    stop(5000); // 5 second timeout
+    z.annotate({element: elem}).using('stanbol').execute().done(function(entities) {
+
+        // console.log(entities);
+
+        ok(entities);
+        //ok(entities.length > 0);
+        //TODO: add more tests
+        start();
+    });
+});
+*/
