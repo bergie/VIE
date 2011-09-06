e95d405c685b5c53c95a6b32f353bf6c55ff6132
Szaby Grünwald <szaby.gruenwald@salzburgresearch.at>
2011-09-06 18:23:02 +0200

    Extending rdfardfquery test by a content="asfd" case

--- test/service/rdfardfquery.js
+++ test/service/rdfardfquery.js
@@ -22,7 +22,7 @@ test("Test simple RDFa parsing", function() {
     var z = new Zart();
     try{
         z.use(new z.RdfaRdfQueryService);
-        var html = jQuery('<div xmlns:dbp="http://dbpedia.org/property/" xmlns:xsd="http://www.w3.org/2001/XMLSchema#" xmlns:foaf="http://xmlns.com/foaf/0.1/" about="http://dbpedia.org/resource/Albert_Einstein"><span property="foaf:name">Albert Einstein</span><span property="dbp:dateOfBirth" datatype="xsd:date">1879-03-14</span><div rel="dbp:birthPlace" resource="http://dbpedia.org/resource/Germany" /><span about="http://dbpedia.org/resource/Germany" property="dbp:conventionalLongName">Federal Republic of Germany</span></div>');
+        var html = jQuery('<div xmlns:dbp="http://dbpedia.org/property/" xmlns:xsd="http://www.w3.org/2001/XMLSchema#" xmlns:foaf="http://xmlns.com/foaf/0.1/" about="http://dbpedia.org/resource/Albert_Einstein"><span property="foaf:name">Albert Einstein</span><span property="dbp:dateOfBirth" datatype="xsd:date" content="1879-03-14">14 march 1879</span><div rel="dbp:birthPlace" resource="http://dbpedia.org/resource/Germany" /><span about="http://dbpedia.org/resource/Germany" property="dbp:conventionalLongName">Federal Republic of Germany</span></div>');
 
         stop(1000); // 1 second timeout
         z.load({element: html}).from('rdfa').execute().done(function(entities) {
@@ -31,8 +31,9 @@ test("Test simple RDFa parsing", function() {
 
             equal(entities[0].id, '<http://dbpedia.org/resource/Albert_Einstein>');
             equal(entities[0].get('foaf:name'), 'Albert Einstein');
+            equal(entities[0].get('dbp:dateOfBirth'), '1879-03-14');
             equal(entities[1].get('dbp:conventionalLongName'), 'Federal Republic of Germany');
-
+            console.info(entities[0].get('dbp:dateOfBirth'));
             start();
         });
     } catch(f){
