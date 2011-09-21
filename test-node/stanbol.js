b147326eb281bc16d988b3590887d96645f8a9ff
Henri Bergius <henri.bergius@iki.fi>
2011-09-21 14:59:37 +0300

    Beginnings of making Stanbol service work on Node

---
+++ test-node/stanbol.js
@@ -0,0 +1,20 @@
+var jQuery = require('jquery');
+var vie = require('../dist/vie-latest.debug.js');
+
+exports['test annotations'] = function(test) {
+    var html = jQuery('<p>This is a small test, where Steve Jobs sings a song.</p>');
+
+    var VIE = new vie.VIE();
+    VIE.use(new VIE.StanbolService, 'stanbol');
+    VIE.analyze({
+            element: html
+        }).
+        using('stanbol').
+        execute().
+        done(function(entities) {
+            test.equal(entities.length, 2);
+        }).
+        then(function() {
+            test.done();
+        });
+};
