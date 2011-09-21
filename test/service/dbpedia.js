5cfa6cb786802683ded4f00ee60749e5c52572c0
Szaby Grünwald <szaby.gruenwald@salzburgresearch.at>
2011-09-21 13:31:21 +0200

    Fixing dbpedia service and add test.

--- test/service/dbpedia.js
+++ test/service/dbpedia.js
@@ -8,16 +8,16 @@ test("Test DBPedia connection", function() {
 });
 
 test("VIE.js DBPediaService - Load", function () {
-    
     var entity = "<http://dbpedia.org/resource/Barack_Obama>";
     var z = new VIE();
     ok (z.DBPediaService);
     equal(typeof z.DBPediaService, "function");
-    z.use(new z.DBPediaService({url : "http://dev.iks-project.eu:8081"}));
+    z.use(new z.DBPediaService());//{url : "http://dev.iks-project.eu:8081"}));
     stop(10000); // 10 second timeout
     z.load({entity: entity})
     .using('dbpedia').execute().done(function(x) {
-        ok(true);
+        ok(x, "Something returned");
+        ok(x[0] instanceof z.Entity, "Returned is an array of VIE Entities?");
         //TODO: add more tests
         start();
     })
