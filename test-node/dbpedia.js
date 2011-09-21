4cc52cefb046dda47e392b551afed147629fe0e6
Henri Bergius <henri.bergius@iki.fi>
2011-09-21 14:28:08 +0300

    Initial DBpedia test for Node

---
+++ test-node/dbpedia.js
@@ -0,0 +1,17 @@
+var vie = require('../dist/vie-latest.debug.js');
+
+exports['test loading Salzburg'] = function(test) {
+    var VIE = new vie.VIE();
+    VIE.use(new VIE.DBPediaService, 'dbpedia');
+    VIE.load({
+        entity: '<http://dbpedia.org/resource/Salzburg>'
+        }).
+        using('dbpedia').
+        execute().
+        done(function(entity) {
+            test.equal(typeof entity, "object");
+        }).
+        then(function() {
+            test.done();
+        });
+};
