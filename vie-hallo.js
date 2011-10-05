249e4dce5b8714dda0d8e3b96c68eeed1023bace
Sebastien Roch <sebastien.roch@liip.ch>
2011-10-05 13:42:29 +0200

    Added function to check if editable was modified for Hallo

--- vie-hallo.js
+++ vie-hallo.js
@@ -10,6 +10,7 @@
             if (!containerInstance) {
                 return;
             }
+
             if (typeof containerInstance.editables === 'undefined') {
                 containerInstance.editables = {};
             }
@@ -23,14 +24,47 @@
                 }
 
                 if (containerInstance.get(propertyName) instanceof Array) {
-                    // For now we don't deal with multivalued properties in Aloha
+                    // For now we don't deal with multivalued properties in Hallo
                     return true;
                 }
 
                 containerInstance.editables[propertyName] = $(this).hallo(opt);
-                //console.log(containerInstance.editables[propertyName]);
                 containerInstance.editables[propertyName].vieContainerInstance = containerInstance;
             });
         });
     };
 })(jQuery);
+
+if (typeof VIE === 'undefined') {
+    VIE = {};
+}
+
+VIE.HalloEditable = {
+    refreshFromEditables: function(objectInstance) {
+        var modifiedProperties = {};
+
+        if (!objectInstance.editables) {
+            return false;
+        }
+
+        // Go through editables of the model instance
+        jQuery.each(objectInstance.editables, function(propertyName, editableInstance) {
+            if (!editableInstance.hallo('isModified')) {
+                // This editable hasn't been modified, skip
+                return true;
+            }
+
+            // Copy editable contents to the modifiedProperties object
+            modifiedProperties[propertyName] = editableInstance.hallo('getContents');
+        });
+
+        if (jQuery.isEmptyObject(modifiedProperties)) {
+            // No modified editables for this object, skip
+            return false;
+        }
+        // Set the modified properties to the model instance
+        objectInstance.set(modifiedProperties);
+
+        return true;
+    }
+};
