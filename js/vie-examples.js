66e5c32bbb72a57bc2b48ab37cc990b8bc8ac800
Henri Bergius <henri.bergius@iki.fi>
2011-11-30 13:30:46 +0200

    First runnable example

--- js/vie-examples.js
+++ js/vie-examples.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function() {
+    jQuery('#features textarea').each(function() {
+        jQuery(this).css('height', jQuery(this).get(0).scrollHeight);
+    });
+
     jQuery('#features').accordion();
+
+    jQuery('#analyze button').button().click(function() {
+        jQuery('#analyze .resultsholder').empty();
+        eval(jQuery('#analyze textarea').val()); 
+    });
 });
