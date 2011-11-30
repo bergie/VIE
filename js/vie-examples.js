ea251c9bd734d456a6bffd4d1379d7bc41aeb79f
Henri Bergius <henri.bergius@iki.fi>
2011-11-30 17:16:46 +0200

    Make the textareas smaller

--- js/vie-examples.js
+++ js/vie-examples.js
@@ -1,8 +1,4 @@
 jQuery(document).ready(function() {
-    jQuery('#features textarea').each(function() {
-        jQuery(this).css('height', jQuery(this).get(0).scrollHeight);
-    });
-
     jQuery('#features').accordion();
     jQuery('#moreinfo').tabs();
 
