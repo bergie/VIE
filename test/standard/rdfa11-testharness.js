67c2e72c2cf893d1053ffa178a26a6f61f5d9447
Sebastian Germesin <neogermi@googlemail.com>
2011-12-01 16:36:55 +0100

    fixing tests for namespaces

--- test/standard/rdfa11-testharness.js
+++ test/standard/rdfa11-testharness.js
@@ -6,7 +6,7 @@ test("Test #1 (required): Predicate establishment with @property", function() {
     var z = new VIE();
     z.use(new z.RdfaService);
     
-    z.namespaces.add("dc", "http://purl.org/dc/terms/");
+    z.namespaces.add("dcterms", "http://purl.org/dc/terms/");
 
     var html = jQuery('<p>This photo was taken by <span class="author" about="photo1.jpg" property="dc:creator">Mark Birbeck</span>.</p>');
 
@@ -27,7 +27,7 @@ test("Test #6 (required): @rel and @rev", function() {
     var z = new VIE();
     z.use(new z.RdfaService);
 
-    z.namespaces.add("dc", "http://purl.org/dc/terms/");
+    z.namespaces.add("dcterms", "http://purl.org/dc/terms/");
     z.namespaces.add("foaf", "http://xmlns.com/foaf/0.1/");
 
     var html = jQuery('<p>This photo was taken by <a about="photo1.jpg" rel="dc:creator" rev="foaf:img" href="http://www.blogger.com/profile/1109404">Mark Birbeck</a>.</p>');
@@ -52,7 +52,7 @@ test("Test #7 (required): @rel, @rev, @property, @content", function() {
     var z = new VIE();
     z.use(new z.RdfaService);
 
-    z.namespaces.add("dc", "http://purl.org/dc/terms/");
+    z.namespaces.add("dcterms", "http://purl.org/dc/terms/");
     z.namespaces.add("foaf", "http://xmlns.com/foaf/0.1/");
     
     var html = jQuery('<p>This photo was taken by <a about="photo1.jpg" property="dc:title" content="Portrait of Mark" rel="dc:creator" rev="foaf:img" href="http://www.blogger.com/profile/1109404">Mark Birbeck</a>.</p>');
@@ -78,7 +78,7 @@ test("Test #9 (required): @rev", function() {
     var z = new VIE();
     z.use(new z.RdfaService);
 
-    z.namespaces.add("dc", "http://purl.org/dc/terms/");
+    z.namespaces.add("dcterms", "http://purl.org/dc/terms/");
     z.namespaces.add("foaf", "http://xmlns.com/foaf/0.1/");
     
     var html = jQuery('<link about="http://example.org/people#Person1" rev="foaf:knows" href="http://example.org/people#Person2" />');
@@ -100,7 +100,7 @@ test("Test #10 (required): @rel, @rev, @href", function() {
     var z = new VIE();
     z.use(new z.RdfaService);
 
-    z.namespaces.add("dc", "http://purl.org/dc/terms/");
+    z.namespaces.add("dcterms", "http://purl.org/dc/terms/");
     z.namespaces.add("foaf", "http://xmlns.com/foaf/0.1/");
     
     var html = jQuery('<link about="http://example.org/people#Person1" rel="foaf:knows" rev="foaf:knows" href="http://example.org/people#Person2" />');
