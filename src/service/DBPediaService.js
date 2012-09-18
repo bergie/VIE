684e217f958d2b9e5ac35466d1d63e9c91d62ee4
Henri Bergius <henri.bergius@iki.fi>
2012-09-18 21:12:05 +0200

    More jshint, variable shadowing

--- src/service/DBPediaService.js
+++ src/service/DBPediaService.js
@@ -119,9 +119,9 @@ VIE.prototype.DBPediaService.prototype = {
                 try {
                     var entities = VIE.Util.rdf2Entities(service, results);
                     entities = (_.isArray(entities))? entities : [ entities ];
-                    for (var e = 0; e < entities.length; e++) {
-                        entities[e].set("DBPediaServiceLoad", VIE.Util.xsdDateTime(new Date()));
-                    }
+                    _.each(entities, function (entity) {
+                        entity.set("DBPediaServiceLoad", VIE.Util.xsdDateTime(new Date()));
+                    });
                     entities = (entities.length === 1)? entities[0] : entities;
                     loadable.resolve(entities);
                 } catch (e) {
