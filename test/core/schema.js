437c63b98bf2d272711e73b393173928b18f1408
Henri Bergius <henri.bergius@iki.fi>
2015-01-21 17:23:11 +0100

    Update to latest schema.org spec

--- test/core/schema.js
+++ test/core/schema.js
@@ -3,7 +3,7 @@ module("vie.js - Schema.org Ontology");
 // Update these when Schema.org ontologies change
 var schemaOrgNumbers = {
   types: 429,
-  hospitalAttribs: 53
+  hospitalAttribs: 56
 };
 
 test("Initialization", function() {
